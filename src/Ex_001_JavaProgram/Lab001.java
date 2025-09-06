2ed8886ba76b0eff29ac523568fef1739f9448fc
Nirmal Singh <nirmal.singh4us@gmail.com>
2025-09-06 23:42:25 +0530

    Twoline added

---
+++ src/Ex_001_JavaProgram/Lab001.java
@@ -0,0 +1,11 @@
+package Ex_001_JavaProgram;
+
+public class Lab001
+{
+    public static void main(String[] args) {
+        System.out.println("Hello Nirmal Singh: This is my first program in Java");
+        System.out.println("Hello Nirmal Singh: This is my first program in Java");
+    }
+
+}
+
---
+++ src/Ex_001_JavaProgram/Lab002_HelloWorld.java
@@ -0,0 +1,7 @@
+package Ex_001_JavaProgram;
+
+public class Lab002_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hi Hello World");
+    }
+}
