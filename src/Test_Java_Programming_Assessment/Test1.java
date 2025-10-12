2a288d025cffbc784c444a6399b1b92ef1047add
Nirmal Singh <nirmal.singh4us@gmail.com>
2025-10-12 11:31:33 +0530

    12oct2025_javatest

---
+++ src/Test_Java_Programming_Assessment/Test1.java
@@ -0,0 +1,15 @@
+package Test_Java_Programming_Assessment;
+
+public class Test1 {
+    public static void main(String[] args) {
+         int Nirmal_Age = 35;
+         double salary = 35.6;
+         boolean is_indian = true;
+         char statecode = 'P';
+
+         System.out.println(Nirmal_Age);
+        System.out.println(salary);
+        System.out.println(is_indian);
+        System.out.println(statecode);
+    }
+}
